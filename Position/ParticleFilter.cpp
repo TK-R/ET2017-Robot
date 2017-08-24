cabc808367b387476bc5ac2179697e9345f877dc
kats-nmt <froe1013@gmail.com>
2017-08-24 21:26:46 +0900

    sinc関数の計算が重いので元に戻した（sinc関数を常に1とする）

--- Position/ParticleFilter.cpp
+++ Position/ParticleFilter.cpp
@@ -51,9 +51,11 @@ void Particle::Update(int8_t leftMotorCount, int8_t rightMotorCount)
 	}
 
 	// 変化量を、正規分布の値を加味して算出
-	double sinc_theta = fabs(theta/2.0)<1.0e-10 ? 1.0 : sin(theta/2.0)/(theta/2.0);
-	double deltaX = D * cos(theta0 + (theta / 2.0)) * sinc_theta;
-	double deltaY = D * sin(theta0 + (theta / 2.0)) * sinc_theta;
+	// double sinc_theta = fabs(theta/2.0)<1.0e-10 ? 1.0 : sin(theta/2.0)/(theta/2.0);
+	// double deltaX = D * cos(theta0 + (theta / 2.0)) * sinc_theta;
+	// double deltaY = D * sin(theta0 + (theta / 2.0)) * sinc_theta;
+	double deltaX = D * cos(theta0 + (theta / 2.0));
+	double deltaY = D * sin(theta0 + (theta / 2.0));
 		
 	RobotPoint.X += deltaX;
 	RobotPoint.Y -= deltaY;
